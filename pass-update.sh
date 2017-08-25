b9969540d5f1838828f3ea88299568d25f167f30
totallycovered <jason@totallycovered.net>
2017-08-25 00:16:02 -0400

    Added getinfo function
    getinfo funtions asks user for info

--- pass-update.sh
+++ pass-update.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-# Source: http://webwiki.co/mass-reset-wordpress-user-passwords/
+# Sources: http://webwiki.co/mass-reset-wordpress-user-passwords/
+#          https://github.com/saadismail/wp-bash
+
+# Get all info from user
+function getinfo(){
+   read -p "Enter database name:" database
+   read -p "Enter Domain.com:" domain
+}
 
 # Update passwords of all wordpress users with random 12 chars passwords & Email them the new passwords
 
-users=$(mysql <dbname> -Bse "select user_login from wp_users")
+users=$(mysql $database -Bse "select user_login from wp_users")
 array=( $users )
 for user in "${array[@]}"
 do
  password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
- mysql <dbname> -e "update wp_users set user_pass = md5('$password') where user_login='$user'"
- email=$(mysql <dbname> -Bse "select user_email from wp_users where user_login='$user'")
- message="Your password for http://example.com/wp-admin has been reset, please login with the following\n\n $user \n $password"
- echo -e $message | mail -s "Example.com Password Reset" $email
+ mysql $database -e "update wp_users set user_pass = md5('$password') where user_login='$user'"
+ email=$(mysql $database -Bse "select user_email from wp_users where user_login='$user'")
+ message="Your password for http://$domain/wp-admin has been reset, please login with the following\n\n $user \n $password"
+ echo -e $message | mail -s "$domain Password Reset" $email
  echo $user $password
 done
